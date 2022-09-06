818ef146b90acc492f4e9ee03737b6290de1ade2
tombeck <t@ocupop.com>
2022-09-06 11:54:27 -0700

    testing absolute urls

--- src/components/Test.client.jsx
+++ src/components/Test.client.jsx
@@ -1,12 +1,15 @@
 import {Image} from '@shopify/hydrogen';
 // ----------------------------------------------------------------------
 
-export default function Test({path = '/images/test.jpg'}) {
+export default function Test({path = './images/test.jpg'}) {
   return (
     <>
       <h1>Test</h1>
       <Image src={path} width="300" height="300" alt="blah" />
-      <img className="product-card__image" src={path} />
+      <img
+        className="product-card__image"
+        src="https://ocupop.com/assets/img/culture/culture-06.jpg"
+      />
     </>
   );
 }
